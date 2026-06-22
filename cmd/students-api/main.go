b46e989f214db2ac552f1120751fbcff015d1cfc
satyam-code45 <satyam45.dev@gmail.com>
2026-06-22 15:44:04 +0530

    graceful-shutdown

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,7 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
-func main()  {
-	fmt.Println("Hello!")
+	"github.com/satyam-code45/students-api/internal/config"
+)
+
+func main() {
+	//load config
+	cfg := config.MustLoad()
+
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to students api"))
+	})
+
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+
+	slog.Info("server started", slog.String("address", cfg.Addr))
+
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func(){
+		err := server.ListenAndServe()
+
+	if err != nil {
+		log.Fatal("Failed to start server")
+	}
+	}()
+
+	<-done
+
+	slog.Info("Shutting down the server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+
+	defer cancel()
+
+	
+	err := server.Shutdown(ctx)
+	if err != nil {
+		slog.Error("Failed to shutdown server", slog.String("error", err.Error()))
+	}
+
+	slog.Info("Server shutdown successfully!")
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -9,13 +9,13 @@ import (
 )
 
 type HTTPServer struct {
-	Addr string
+	Addr string `yaml:"address" env-required:"true"`
 }
 
 type Config struct {
 	Env string  `yaml:"env" env:"ENV" env-required:"true"`
-	StorgePath string `yaml:"storage_path" env-required:"true"`
-	HTTPServer `yaml:"html_server"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer `yaml:"http_server"`
 }
 
 func MustLoad() *Config{
