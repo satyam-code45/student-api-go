ebd7d8f0ddddca88b50deda3b51fdae80ecbb8a9
satyam-code45 <satyam45.dev@gmail.com>
2026-06-22 15:21:41 +0530

    parsed-config-file

---
+++ internal/config/config.go
@@ -0,0 +1,53 @@
+package config
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type HTTPServer struct {
+	Addr string
+}
+
+type Config struct {
+	Env string  `yaml:"env" env:"ENV" env-required:"true"`
+	StorgePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer `yaml:"html_server"`
+}
+
+func MustLoad() *Config{
+	var configPath string
+
+	configPath = os.Getenv("CONFIG_PATH")
+
+	if configPath == ""{
+		flags := flag.String("config", "", "path to the configuration file")
+		flag.Parse()
+
+		configPath = *flags
+
+		if configPath == "" {
+			log.Fatal("Config path is not set")
+		}
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err){
+		log.Fatalf("Config file does not exists: %s", configPath)
+	}
+
+	var cfg Config
+
+
+
+
+	err := cleanenv.ReadConfig(configPath, &cfg)
+
+	if err != nil {
+		log.Fatalf("Cannot read config files: %s", err.Error())
+	}
+
+	return &cfg
+}
