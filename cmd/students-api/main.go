e5173e4d0ae74fa6d144a5aabbe745ecf22be67b
satyam-code45 <satyam45.dev@gmail.com>
2026-06-22 19:13:18 +0530

    added-request-validation

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -11,6 +11,7 @@ import (
 	"time"
 
 	"github.com/satyam-code45/students-api/internal/config"
+	"github.com/satyam-code45/students-api/internal/http/handlers/student"
 )
 
 func main() {
@@ -19,9 +20,7 @@ func main() {
 
 	router := http.NewServeMux()
 
-	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Welcome to students api"))
-	})
+	router.HandleFunc("POST /api/students", student.New())
 
 	server := http.Server{
 		Addr:    cfg.Addr,
